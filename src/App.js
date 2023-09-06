0c3e5e910c90cebd00e2c983eabe64fadb2c5298
andreyzharov <monotank23@gmail.com>
2023-09-06 23:07:27 +0300

    update back

--- src/App.js
+++ src/App.js
@@ -8,51 +8,22 @@ import ClockPage from './Pages/ClockPage';
 import MorseDinoGame from './Pages/MorseDinoGame';
 import PageFive from "./Pages/PageFive";
 
+
+
+
 function App() {
-    const [isMobile, setIsMobile] = useState(false);
-
-    useEffect(() => {
-        // Проверяем userAgent для определения типа устройства
-        const userAgent = navigator.userAgent.toLowerCase();
-
-        if (
-            userAgent.includes('android') ||
-            userAgent.includes('iphone') ||
-            userAgent.includes('ipad')
-        ) {
-            setIsMobile(true);
-        } else {
-            setIsMobile(false);
-        }
-    }, []);
-
-    return (
-        <Router>
-            <Routes>
-                {isMobile ? (
-                    <Route
-                        path="/"
-                        element={
-                            <div>
-                                <p>
-                                    Зайдите через компьютер по этой <a href="https://reactstart-zeta.vercel.app">ссылке</a>
-                                </p>
-                            </div>
-                        }
-                    />
-                ) : (
-                    <>
-                        <Route path="/" element={<Password />} />
-                        <Route path="/labyrinth" element={<LabyrinthPage />} />
-                        <Route path="/clock" element={<ClockPage />} />
-                        <Route path="/dino" element={<MorseDinoGame />} />
-                        <Route path="/second" element={<SecondPage />} />
-                        <Route path="/pagefive" element={<PageFive />} />
-                    </>
-                )}
-            </Routes>
-        </Router>
-    );
+  return (
+      <Router >
+          <Routes>
+              <Route path="/" element={<Password />} />
+              <Route path="/labyrinth" element={<LabyrinthPage />} />
+              <Route path="/clock" element={<ClockPage />} />
+              <Route path="/dino" element={<MorseDinoGame />} />
+              <Route path="/second" element={<SecondPage />} />
+              <Route path="/pagefive" element={<PageFive />} />
+          </Routes>
+      </Router>
+  );
 }
 
 export default App;
